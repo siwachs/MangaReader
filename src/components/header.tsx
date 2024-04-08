95133fa52b40150d1f5fbcacd51f3b0a8632c292
siwachs <ssiwach.8888@gmail.com>
2024-04-08 16:28:28 +0530

    Language Picker

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,20 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { desktopNavLinks } from "@/data/navbarNavlinks";
+import ChevronDown from "./icons/ChevronDown";
+
+const languages: {
+  name: string;
+  href: string;
+  dataLink: string;
+  dataLanguageCode: string;
+}[] = [
+  { name: "English", href: "/en", dataLink: "/en", dataLanguageCode: "en" },
+  { name: "Indonesia", href: "/id", dataLink: "/id", dataLanguageCode: "id" },
+  { name: "Español", href: "/es", dataLink: "/es", dataLanguageCode: "es" },
+  { name: "Português", href: "/pt", dataLink: "/pt", dataLanguageCode: "pt" },
+  { name: "ไทย", href: "/th", dataLink: "/th", dataLanguageCode: "th" },
+];
 
 const Header: React.FC = () => {
   return (
@@ -30,7 +44,7 @@ const Header: React.FC = () => {
         </nav>
 
         <nav>
-          <div className="flex items-center gap-4 text-sm font-semibold">
+          <div className="flex items-center gap-5 text-sm font-semibold">
             <Link className="text-[var(--app-navlink-color)]" href="/">
               Log In
             </Link>
@@ -38,8 +52,34 @@ const Header: React.FC = () => {
               History
             </Link>
 
-            <div id="en">
-              <div></div>
+            <div>
+              <label
+                htmlFor="language-picker"
+                className="flex cursor-pointer select-none items-center gap-1 text-[var(--app-text-color-dark-gray)]"
+              >
+                <span>English</span>
+                <ChevronDown />
+              </label>
+              <input type="checkbox" hidden id="language-picker" />
+
+              <div
+                id="languages-list"
+                className="absolute -top-[100%] w-40 rounded-[10px] border border-[var(--app-border-color-gray)] bg-white py-2.5 transition-all"
+              >
+                {languages.map((language) => (
+                  <Link
+                    data-link={language.dataLink}
+                    data-language-code={language.dataLanguageCode}
+                    href={language.href}
+                    key={language.name}
+                    className={`inline-block h-7 w-full px-4 py-1 ${language.name === "English" && "bg-red-200"}`}
+                  >
+                    <span className="ml-2.5 text-sm text-[var(--app-text-color-dark-gray)]">
+                      {language.name}
+                    </span>
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
 
