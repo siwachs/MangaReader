6d6581248fd81c866928f271b3a69e137221ea79
siwachs <ssiwach.8888@gmail.com>
2024-04-20 20:25:53 +0530

    Content Info Page

--- src/app/[title]/page.tsx
+++ src/app/[title]/page.tsx
@@ -1,12 +1,37 @@
 import type { Metadata } from "next";
+import React from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Like, StarSolid, View } from "@/components/icons";
 
 const data = {
+  thumbnail: "/dummyContent/thumbnail.webp",
+  poster: "/dummyContent/3.webp",
   title: "The Beginner's Guide to Be A Princess",
+  status: "on going",
+  genres: [
+    "Shonen",
+    "Shojo",
+    "Seinen",
+    "Josei",
+    "Isekai",
+    "Slice of Life",
+    "Fantasy",
+    "Sci-Fi",
+    "Horror",
+    "Mystery",
+    "Romance",
+    "Action",
+  ],
+  noOfViews: "6.2M",
+  numberOfLikes: "345k",
+  rating: 4.5,
   description: `All the infant girls countrywide drift downstream from the beginning of the fosse. Whoever reaches the terminal point first will be adopted by the emperor.
   Excuse me??? Who think of such ridiculous idea?! Whoa! I want to go back to my original world!
   What an awful time travel!
   MangaToon got authorization from Xiaomingtaiji to publish this work, the content is the author's own point of view, and does not represent the stand of MangaToon.`,
-  thumbnail: "/dummyContent/thumbnail.webp",
 };
 
 export const metadata: Metadata = {
@@ -15,5 +40,90 @@ export const metadata: Metadata = {
 };
 
 export default function TitlePage() {
-  return <></>;
+  return (
+    <>
+      <div className="details-wrapper mx-[5%] w-[90%] max-w-[1200px] pt-[5%] lg:mx-auto lg:pt-[45px]">
+        <div className="details-top-info mx-[1px] mb-[1px] h-[27%] w-full overflow-visible lg:h-[345px] lg:overflow-hidden">
+          <div className="detail-image relative mr-[45px] h-[54.5vw] w-full lg:inline-block lg:h-[345px] lg:w-[260px]">
+            <Image
+              quality={100}
+              src={data.thumbnail}
+              alt={data.title}
+              fill
+              className="h-full w-full rounded object-cover object-center lg:hidden"
+              sizes="(max-width: 1024px) 60vw, 54vw"
+            />
+            <Image
+              quality={100}
+              src={data.poster}
+              alt={data.title}
+              fill
+              className="hidden h-full w-full rounded object-cover object-center lg:block"
+              sizes="(max-width: 1024px) 60vw, 54vw"
+            />
+          </div>
+
+          <div className="detail-info inline-block w-full overflow-hidden lg:h-[345px] lg:w-[880px]">
+            <div className="detail-title flex overflow-hidden py-2.5">
+              <span
+                style={{ fontFamily: "Noto Sans SC" }}
+                className="hide-text mr-[80px] w-full text-lg/[24px] font-[500]"
+              >
+                {data.title}
+              </span>
+              <div className="absolute right-[5%] box-content h-[18px] whitespace-nowrap rounded-xl bg-[var(--app-text-color-red-orange)] px-[9px] pb-[3px] pt-[2px] text-center text-xs/[18px] text-white">
+                {data.status}
+              </div>
+            </div>
+
+            <div
+              style={{ fontFamily: "Noto Sans SC" }}
+              className="detail-genres mb-[5px] h-5 overflow-auto whitespace-nowrap text-xs/[20px] font-normal text-[var(--app-text-color-darker-gray)] lg:mb-[15px] lg:w-[700px] lg:text-sm lg:text-[var(--app-text-color-slate-gray)]"
+            >
+              {data.genres.map((genre, index) => (
+                <React.Fragment key={genre}>
+                  <Link href={"/"} key={genre}>
+                    {genre}
+                  </Link>
+                  {index + 1 !== data.genres.length && " / "}
+                </React.Fragment>
+              ))}
+            </div>
+
+            <div className="detail-stats flex items-center text-[13px] text-[var(--app-text-color-darker-gray)]">
+              <View className="-mt-0.5 mr-0.5 h-[13px] w-[13px]" />
+              <span>{data.noOfViews}</span>
+
+              <Like className="-mt-0.5 ml-2.5 mr-0.5 h-[14px] w-[14px]" />
+              <span>{data.numberOfLikes}</span>
+
+              <div className="-mt-0.5 ml-2.5 flex items-center overflow-hidden">
+                {[...new Array(5)].map((star, index) => {
+                  const uniqueKey = `star${index}`;
+                  const inverse = Math.floor(data.rating) <= index;
+                  return (
+                    <StarSolid
+                      key={uniqueKey}
+                      inverse={inverse}
+                      className="h-[17px] w-[17px] lg:mr-[3px]"
+                    />
+                  );
+                })}
+
+                <span className="-mb-[1px] ml-[4px] mt-[1px] text-sm">
+                  {data.rating}
+                </span>
+              </div>
+            </div>
+
+            <div className="detail-author"></div>
+
+            <div className="detail-description"></div>
+          </div>
+        </div>
+      </div>
+
+      <div></div>
+    </>
+  );
 }
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -418,6 +418,56 @@ const Heart: React.FC<{ className?: string }> = ({ className = "h-5 w-5" }) => {
   );
 };
 
+const StarSolid: React.FC<{ className?: string; inverse?: boolean }> = ({
+  className = "h-5 w-5",
+  inverse = false,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill={inverse ? "#d8d8d8" : "#ffbd4f"}
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+const Pencil: React.FC<{
+  className?: string;
+  strokeWidth?: number;
+  onClick?: () => void;
+}> = ({ className = "h-5 w-5", strokeWidth = 1.5, onClick }) => {
+  return (
+    <svg
+      tabIndex={0}
+      onKeyDown={(e: React.KeyboardEvent<SVGSVGElement>) => {
+        if (e.key === "Enter" && onClick) {
+          onClick();
+        }
+      }}
+      onClick={onClick}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+      />
+    </svg>
+  );
+};
+
 export {
   ChevronDown,
   ChevronLeft,
@@ -436,4 +486,6 @@ export {
   Like,
   View,
   Heart,
+  StarSolid,
+  Pencil,
 };
